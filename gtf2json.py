b2b578495f4d214e1021c60d802597e87f4fcbfc
Kayley Wong <kayleymanyeewong@gmail.com>
2020-09-09 00:33:00 -0700

    third

---
+++ gtf2json.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import sys
+import fileinput
+import csv
+import json
+
+gtf_file = sys.argv[1]
+output=[]
+for each_line_of_text in fileinput.input(gtf_file):
+    if each_line_of_text.startswith('#'):
+        continue
+    line = each_line_of_text.split("\t")
+    
+    if line[2] == 'gene':
+        s = next(csv.reader([line[8]], delimiter=' '))
+        record = {}
+        record["geneName"] = (s[3]).rstrip(";")
+        record["chr"] = line[0]
+        record["startPos"] = line[3]
+        record["endPos"] = line[4]
+
+        output.append(record)
+        
+y= json.dumps(output,indent=2)
+print(y)
